15509cef9fd1ca39d3add0750f827d461a75d7e0
Paul-Adrien <paul-adrien-for-ever@hotmail.fr>
2022-10-30 16:37:49 +0100

    javascript for dropdown selection

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -4,8 +4,61 @@ import { photographerList } from '../factories/photographer.js';
 let photographerID = new URLSearchParams(document.location.search).get('id');
 let myPagePhotograph = [];
 
-// Create array with photographe clicked
+const filterSelect = document.querySelector('.filter-select');
+const filterSelectTop = document.querySelector('.filter-select-top');
+const filterOption = document.querySelectorAll('.filter-option');
 
-// class Photographe {
-//   constructor() {}
-// }
+function openSortFilter(e) {
+  filterSelect.classList.toggle('open');
+
+  filterSelectTop.ariaExpanded === 'false'
+    ? (filterSelectTop.ariaExpanded = 'true')
+    : (filterSelectTop.ariaExpanded = false);
+}
+
+function handleDropdownSelection(e) {
+  const filterChosen = document.querySelector('.chosen');
+
+  let clickedValue = e.target;
+
+  if (e.target.textContent === 'Date') {
+    filterOption.forEach((options) => {
+      options.classList.remove('selected');
+    });
+
+    clickedValue.classList.add('selected');
+
+    filterChosen.textContent = e.target.textContent;
+
+    console.log('Sort by date');
+  } else if (e.target.textContent === 'Titre') {
+    filterOption.forEach((options) => {
+      options.classList.remove('selected');
+    });
+    clickedValue.classList.add('selected');
+
+    filterChosen.textContent = e.target.textContent;
+
+    console.log('Sort by Titre');
+  } else if (e.target.textContent === 'Popularité') {
+    filterOption.forEach((options) => {
+      options.classList.remove('selected');
+    });
+
+    clickedValue.classList.add('selected');
+
+    filterChosen.textContent = e.target.textContent;
+
+    console.log('Sort by Popularité');
+  } else {
+    return;
+  }
+}
+
+// Event Listeners
+
+filterSelect.addEventListener('click', openSortFilter);
+
+for (const filter of filterOption) {
+  filter.addEventListener('click', handleDropdownSelection);
+}
